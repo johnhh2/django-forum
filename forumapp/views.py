c8074fdb2802eea6ab9dce31a8ecee99a3411e8a
Matt Purnell <matt.purnell98@gmail.com>
2020-05-29 12:52:08 -0500

    Added base.html and started views

--- forumapp/views.py
+++ forumapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+class ChannelView(generic.ListView):
+    model = Channel
+    template_name = 'forumapp/channel.html'
+
+class ThreadView(generic.DetailView):
+    model = Thread
+    template_name = 'forumapp/thread.html'
+
+class CommentView(generic.DetailView):
+    model = Comment
+    template_name = 'forumapp/comment.html'
