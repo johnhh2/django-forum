0c4b482e93a1f345708837120e00e75b02a898a0
johnhh2 <johnhh2@illinois.edu>
2020-05-29 18:58:11 -0500

    Added Comment tests

--- forumapp/tests.py
+++ forumapp/tests.py
@@ -7,20 +7,18 @@ from .models import Channel, Thread, Comment
 
 
 ## Helper functions
-def create_user(name):
-    return User.objects.create(username=name)
-
-def get_user(name):
-    return User.objects.get(username=name)
 
 def create_channel(name, owner, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Channel.objects.create(channel_name=name, owner=owner, pub_date=time)
 
+def create_comment(thread, text, owner, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Comment.objects.create(thread_id=thread, text=text, owner=owner, pub_date=time)
+
 ## Channel tests
 class ChannelModelTests(TestCase):
     name = "Test channel 123456789"
-    owner = get_user("testuser2")
 
     def testNoChannel(self):
         response = self.client.get(reverse('forumapp:channel'))
@@ -29,11 +27,86 @@ class ChannelModelTests(TestCase):
         self.assertQuerysetEqual(response.context['channel_list'], [])
 
     def testChannelCreateDelete(self):
-        c = create_channel(self.name, self.owner, -1)
-        
-        self.assertIn(c.channel_name, self.name)
-        self.assertEqual(len(c.channel_name), len(self.name))
+        owner = User.objects.create(self.name)
 
+        c = create_channel(self.name, owner, -1)
+
+        response = self.client.get(reverse('forumapp:channel'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.name)
+        
         c.delete()
 
         self.testNoChannel()
+
+    def testChannelsAreDisplayed(self):
+        owner = User.objects.create(self.name)
+
+        name2 = "testchannel2"
+        c = create_channel(self.name, owner, -1)
+        c = create_channel(name2, owner, -1)
+
+        response = self.client.get(reverse('forumapp:channel'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.name)
+        self.assertContains(response, name2)
+
+
+## Comment tests
+class CommentModelTests(TestCase):
+    owner_name = "testuser3"
+    owner_name2 = "testuser4"
+    channel_name = "aaatestchannel"
+    thread_name = "stupid topic"
+
+    def testNoComment(self):
+        owner = User.objects.create(self.self.owner_name)
+        channel = create_channel(self.channel_name, owner, -2)
+        owner2 = User.objects.create(self.owner_name2)
+        thread = create_thread(channel, self.thread_name, owner2, -1)
+
+        response = self.client.get(reverse('forumapp:comment'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No comments are available.")
+        self.assertQuerysetEqual(response.context['comment_list'], [])
+
+    def testCommentCreateDelete(self):
+        owner = User.objects.create(self.owner_name)
+        channel = create_channel(self.channel_name, owner, -2)
+        owner2 = User.objects.create(self.owner_name2)
+        thread = create_thread(channel, self.thread_name, owner2, -1)
+        text = "test text :)"
+        owner3 = User.objects.create(self.owner_name3)
+
+        c = create_comment(thread, text, owner3, -1)
+
+        response = self.client.get(reverse('forumapp:comment'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, text)
+        #self.assertContains(response, self.owner_name3)
+        
+        c.delete()
+
+        self.testNoComment()
+
+    def testCommentsAreDisplayed(self):
+        name2 = "testchannel2"
+        owner = User.objects.create(self.owner_name)
+        channel = create_channel(self.channel_name, owner, -2)
+        owner2 = User.objects.create(self.owner_name2)
+        thread = create_thread(channel, self.thread_name, owner2, -1)
+        text = "test text :)"
+        owner3 = User.objects.create(self.owner_name3)
+
+        c = create_comment(thread, text, owner3, 0)
+        c = create_comment(thread, text[::-1], owner2, -1)
+
+        response = self.client.get(reverse('forumapp:comment'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, text)
+        self.assertContains(response, text[::-1])
+
