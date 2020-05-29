f00993234fc37189d510e4faf324de6c0280fc1b
johnhh2 <johnhh2@illinois.edu>
2020-05-29 13:01:20 -0500

    Implemented database model

--- forumapp/models.py
+++ forumapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Channel(models.Model):
+    channel_name = models.CharField(max_length=30, primary_key=True)
+
+    owner = models.ForeignKey(User, to_field="username")
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.channel_name
+
+# Store thread_id as primary key
+class Thread(models.Model):
+    thread_id = models.IntegerField(primary_key=True)
+    channel_name = models.ForeignKey(Channel, to_field="channel_name")
+
+    thread_name = models.CharField(max_length=90)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.thread_name
+
+# Primary keys are thread_name and comment_id where comment_id starts at 1 for every new thread
+class Comment(models.Model):
+    comment_id = models.IntegerField(primary_key=True)
+    thread_id = models.ForeignKey(Thread, to_field="thread_id")
+
+    pub_date = models.DateTimeField('date published')
+
+    class Meta:
+        unique_together = [["thread_id", "comment_id"]] # 2 primary keys
+
+    def __str__(self):
+        return self.text
--- forumapp/urls.py
+++ forumapp/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url(r'^foundation', include('foundation.urls')),
+    url(r'^foundation/', include('foundation.urls')),
 ]
