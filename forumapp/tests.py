407b01334c1a822a31e9f50092efc79bb9477dd4
johnhh2 <johnhugh1998@gmail.com>
2020-06-03 17:07:41 -0500

    Reorganized deletion tests, added test class for User

--- forumapp/tests.py
+++ forumapp/tests.py
@@ -38,7 +38,7 @@ def create_comment(thread, owner, text="text", days=0):
 ## Channel tests
 class ChannelTests(TestCase):
     channel_name = "Test-channel-123456789"
-    owner_name = 'owner'
+    username = 'owner'
 
     def testNoChannel(self):
         response = self.client.get(reverse('forumapp:channel'))
@@ -48,7 +48,7 @@ class ChannelTests(TestCase):
         self.assertQuerysetEqual(response.context['channel_list'], [])
 
     def testChannelCreateDelete(self):
-        owner = User.objects.create(username=self.owner_name)
+        owner = User.objects.create(username=self.username)
 
         c = create_channel(self.channel_name, owner)
 
@@ -65,7 +65,7 @@ class ChannelTests(TestCase):
         self.testNoChannel()
 
     def testChannelsAreDisplayed(self):
-        owner = User.objects.create(username=self.owner_name)
+        owner = User.objects.create(username=self.username)
 
         channel_name_2 = "testchannel2"
         c = create_channel(self.channel_name, owner)
@@ -76,16 +76,35 @@ class ChannelTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.channel_name)
         self.assertContains(response, channel_name_2)
+    
+    ## Tests whether a channel is correctly passed off to one of the channel moderators
+    ## if a channel moderator is specified when the owner is deleted
+    def testChannelOwnerPassOff(self):
+        owner = User.objects.create(username=self.username)
+        otheruser = User.objects.create(username=self.username[::-1])
+
+        channel = create_channel(self.channel_name, owner)
+        subthread = create_thread(channel, owner)
+
+        # set moderator
+        channel.moderators = json.dumps([otheruser.username])
+        channel.save()
+
+        #delete channel and see if the owner was changed to to the otheruser
+        owner.delete()
+
+        self.assertEqual(True, Channel.objects.filter(channel_name=self.channel_name).exists())
+        self.assertEqual(self.username[::-1], Channel.objects.get(channel_name=self.channel_name).owner.username)
 
 ## Thread tests
 class ThreadTests(TestCase):
     channel_name = "channelforthreadtest"
-    owner_name = 'owner'
+    username = 'owner'
     thread_name = "threadtest"
     thread_desc = "descriptionforthreadtest"
 
     def testNoThread(self):
-        owner = User.objects.create(username=self.owner_name)
+        owner = User.objects.create(username=self.username)
         c = create_channel(self.channel_name, owner)
 
         response = self.client.get(reverse('forumapp:thread', kwargs={'channel': self.channel_name}))
@@ -95,7 +114,7 @@ class ThreadTests(TestCase):
         self.assertQuerysetEqual(response.context['thread_list'], [])
 
     def testThreadCreateDelete(self):
-        owner = User.objects.create(username=self.owner_name)
+        owner = User.objects.create(username=self.username)
         c = create_channel(self.channel_name, owner)
         t = create_thread(c, owner, self.thread_name, self.thread_desc)
 
@@ -116,7 +135,7 @@ class ThreadTests(TestCase):
         self.assertQuerysetEqual(response.context['thread_list'], [])
 
     def testThreadsAreDisplayed(self):
-        owner = User.objects.create(username=self.owner_name+'2')
+        owner = User.objects.create(username=self.username+'2')
         c = create_channel(self.channel_name, owner, -1)
         t1 = create_thread(c, owner, self.thread_name, self.thread_desc)
         t2 = create_thread(c, owner, self.thread_name[::-1], self.thread_desc)
@@ -129,159 +148,6 @@ class ThreadTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.thread_name)
         self.assertContains(response, self.thread_name[::-1])
-
-## Comment tests
-class CommentTests(TestCase):
-    owner_name = "testuser3"
-    owner_name2 = "testuser4"
-    owner_name3 = "testuser5"
-    channel_name = "aaatestchannel"
-    text = "test text :)"
-
-    def testNoComment(self):
-        owner = User.objects.create(username=self.owner_name)
-
-        channel = create_channel(self.channel_name, owner)
-        owner2 = User.objects.create(username=self.owner_name2)
-        thread = create_thread(channel, owner2)
-
-        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': channel.channel_name, 'thread': thread.thread_id}))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No comments are available.")
-        self.assertQuerysetEqual(response.context['comment_list'], [])
-
-    def testCommentCreateDelete(self):
-        owner = User.objects.create(username=self.owner_name)
-
-        channel = create_channel(self.channel_name, owner)
-        owner2 = User.objects.create(username=self.owner_name2)
-        thread = create_thread(channel, owner2)
-        owner3 = User.objects.create(username=self.owner_name3)
-
-        c = create_comment(thread, owner3, self.text)
-
-        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.text)
-        self.assertContains(response, self.owner_name3)
-
-        c.delete()
-
-        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No comments are available.")
-        self.assertQuerysetEqual(response.context['comment_list'], [])
-
-    def testCommentsAreDisplayed(self):
-        owner = User.objects.create(username=self.owner_name)
-        channel = create_channel(self.channel_name, owner)
-        owner2 = User.objects.create(username=self.owner_name2)
-        thread = create_thread(channel, owner2)
-        owner3 = User.objects.create(username=self.owner_name3)
-
-
-        c1 = create_comment(thread, owner3, self.text)
-        c2 = create_comment(thread, owner2, self.text[::-1])
-
-        self.assertEquals(0, c1.comment_id)
-        self.assertEquals(1, c2.comment_id)
-
-        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.text)
-        self.assertContains(response, self.text[::-1])
-
-    # TODO: add test for is_recent
-    def testCommentIsRecent(self):
-        owner = User.objects.create(username=self.owner_name)
-        channel = create_channel(self.channel_name, owner)
-        owner2 = User.objects.create(username=self.owner_name2)
-        thread = create_thread(channel, owner2)
-        owner3 = User.objects.create(username=self.owner_name3)
-
-        c1 = create_comment(thread, owner3, days=-2)
-        c2 = create_comment(thread, owner3, days=-0.5)
-        c3 = create_comment(thread, owner2, days=0)
-        c4 = create_comment(thread, owner, days=1)
-
-        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
-
-        self.assertEqual(response.status_code, 200)
-        self.assertIs(c1.is_recent(), False)
-        self.assertIs(c2.is_recent(), True)
-        self.assertIs(c3.is_recent(), True)
-        self.assertIs(c4.is_recent(), False)
-
-## Preserve data when a User is deleted
-## Cascade-delete when channels/threads are removed (preserving users)
-class CascadeDeleteTests(TestCase):
-    username = "randomuser91387245"
-    channel_name = "channel1981719"
-    channel_name2 = "asfsga"
-    channel_name3 = "nhjtgrew"
-    thread_name = "aaa"
-    thread_name2 = "bbb"
-    thread_name3 = "ccc"
-    def testUserDelete(self):
-        owner = User.objects.create(username=self.username)
-        otheruser = User.objects.create(username=self.username[::-1])
-
-        # Create a channel
-        channel = create_channel(self.channel_name, owner)
-        subthread = create_thread(channel, owner, self.thread_name)
-        thread_id1 = subthread.thread_id
-
-        subcomment = create_comment(subthread, owner)
-        comment_id1 = subcomment.comment_id
-
-        # Create a thread under another users channel
-        otherchannel = create_channel(self.channel_name2, otheruser)
-        thread = create_thread(otherchannel, owner, self.thread_name2)
-
-
-        # Create a comment under another users channel and thread
-        otherthread = create_thread(otherchannel, otheruser, self.thread_name3)
-        thread_id2 = otherthread.thread_id
-
-        otherthread_id = otherthread.thread_id
-        comment = create_comment(otherthread, owner)
-        comment_id2 = comment.comment_id
-
-        owner.delete()
-
-        # Channel and its children should be gone (this tests cascade deletion
-        #   for channels->threads->comments)
-        self.assertEqual(False, Channel.objects.filter(owner=None).exists())
-        self.assertEqual(False, Thread.objects.filter(channel=None).exists())
-        self.assertEqual(False, Comment.objects.filter(thread=None).exists())
-
-        # Verify that the thread and comment under the other user's channel should still exist
-        self.assertEqual(True, Channel.objects.filter(channel_name=self.channel_name2).exists())
-        self.assertEqual(True, Thread.objects.filter(channel__channel_name=self.channel_name2, thread_id=thread_id2).exists())
-        self.assertEqual(True, Comment.objects.filter(thread__channel__channel_name=self.channel_name2, thread__thread_id=otherthread_id, comment_id=comment_id2).exists())
-
-    ## Tests whether a channel is correctly passed off to one of the channel moderators
-    ## if a channel moderator is specified when the owner is deleted
-    def testChannelOwnerPassOff(self):
-        owner = User.objects.create(username=self.username)
-        otheruser = User.objects.create(username=self.username[::-1])
-
-        channel = create_channel(self.channel_name, owner)
-        subthread = create_thread(channel, owner, self.thread_name)
-
-        # set moderator
-        channel.moderators = json.dumps([otheruser.username])
-        channel.save()
-
-        #delete channel and see if the owner was changed to to the otheruser
-        owner.delete()
-
-        self.assertEqual(True, Channel.objects.filter(channel_name=self.channel_name).exists())
-        self.assertEqual(self.username[::-1], Channel.objects.get(channel_name=self.channel_name).owner.username)
 
     ## Test whether deleting a thread preserves its channel and deletes its comments
     def testThreadDelete(self):
@@ -304,13 +170,78 @@ class CascadeDeleteTests(TestCase):
         self.assertEquals(False, Comment.objects.filter(thread__thread_id=thread_id, comment_id=comment_id).exists())
         self.assertEquals(False, Comment.objects.filter(thread=None).exists())
     
+## Comment tests
+class CommentTests(TestCase):
+    username = "testuser3"
+    username2 = "testuser4"
+    username3 = "testuser5"
+    channel_name = "aaatestchannel"
+    text = "test text :)"
+
+    def testNoComment(self):
+        owner = User.objects.create(username=self.username)
+
+        channel = create_channel(self.channel_name, owner)
+        owner2 = User.objects.create(username=self.username2)
+        thread = create_thread(channel, owner2)
+
+        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': channel.channel_name, 'thread': thread.thread_id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No comments are available.")
+        self.assertQuerysetEqual(response.context['comment_list'], [])
+
+    def testCommentCreateDelete(self):
+        owner = User.objects.create(username=self.username)
+
+        channel = create_channel(self.channel_name, owner)
+        owner2 = User.objects.create(username=self.username2)
+        thread = create_thread(channel, owner2)
+        owner3 = User.objects.create(username=self.username3)
+
+        c = create_comment(thread, owner3, self.text)
+
+        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.text)
+        self.assertContains(response, self.username3)
+
+        c.delete()
+
+        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No comments are available.")
+        self.assertQuerysetEqual(response.context['comment_list'], [])
+
+    def testCommentsAreDisplayed(self):
+        owner = User.objects.create(username=self.username)
+        channel = create_channel(self.channel_name, owner)
+        owner2 = User.objects.create(username=self.username2)
+        thread = create_thread(channel, owner2)
+        owner3 = User.objects.create(username=self.username3)
+
+
+        c1 = create_comment(thread, owner3, self.text)
+        c2 = create_comment(thread, owner2, self.text[::-1])
+
+        self.assertEquals(0, c1.comment_id)
+        self.assertEquals(1, c2.comment_id)
+
+        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.text)
+        self.assertContains(response, self.text[::-1])
+    
     ## Test whether deleting a comment preserves its thread
     def testCommentDelete(self):
         owner = User.objects.create(username=self.username)
         
         # Create a channel
         channel = create_channel(self.channel_name, owner)
-        thread = create_thread(channel, owner, self.thread_name)
+        thread = create_thread(channel, owner)
         thread_id = thread.thread_id
 
         comment = create_comment(thread, owner)
@@ -323,6 +254,72 @@ class CascadeDeleteTests(TestCase):
         self.assertEquals(True, Thread.objects.filter(channel__channel_name=self.channel_name, thread_id=thread_id).exists())
         self.assertEquals(False, Comment.objects.filter(thread__thread_id=thread_id, comment_id=comment_id).exists())
         self.assertEquals(False, Comment.objects.filter(thread=None).exists())
+
+    def testCommentIsRecent(self):
+        owner = User.objects.create(username=self.username)
+        channel = create_channel(self.channel_name, owner)
+        owner2 = User.objects.create(username=self.username2)
+        thread = create_thread(channel, owner2)
+        owner3 = User.objects.create(username=self.username3)
+
+        c1 = create_comment(thread, owner3, days=-2)
+        c2 = create_comment(thread, owner3, days=-0.5)
+        c3 = create_comment(thread, owner2, days=0)
+        c4 = create_comment(thread, owner, days=1)
+
+        response = self.client.get(reverse('forumapp:comment', kwargs={'channel': self.channel_name, 'thread': thread.thread_id}))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIs(c1.is_recent(), False)
+        self.assertIs(c2.is_recent(), True)
+        self.assertIs(c3.is_recent(), True)
+        self.assertIs(c4.is_recent(), False)
+
+class UserTests(TestCase):
+    username = "randomuser91387245"
+    channel_name = "channel1981719"
+    channel_name2 = "asfsga"
+
+    ## Preserve data when a User is deleted
+    ## Cascade-delete when channels/threads are removed (preserving users)
+    def testUserDelete(self):
+        owner = User.objects.create(username=self.username)
+        otheruser = User.objects.create(username=self.username[::-1])
+
+        # Create a channel
+        channel = create_channel(self.channel_name, owner)
+        subthread = create_thread(channel, owner)
+        thread_id1 = subthread.thread_id
+
+        subcomment = create_comment(subthread, owner)
+        comment_id1 = subcomment.comment_id
+
+        # Create a thread under another users channel
+        otherchannel = create_channel(self.channel_name2, otheruser)
+        thread = create_thread(otherchannel, owner)
+
+
+        # Create a comment under another users channel and thread
+        otherthread = create_thread(otherchannel, otheruser)
+        thread_id2 = otherthread.thread_id
+
+        otherthread_id = otherthread.thread_id
+        comment = create_comment(otherthread, owner)
+        comment_id2 = comment.comment_id
+
+        owner.delete()
+
+        # Channel and its children should be gone (this tests cascade deletion
+        #   for channels->threads->comments)
+        self.assertEqual(False, Channel.objects.filter(owner=None).exists())
+        self.assertEqual(False, Thread.objects.filter(channel=None).exists())
+        self.assertEqual(False, Comment.objects.filter(thread=None).exists())
+
+        # Verify that the thread and comment under the other user's channel should still exist
+        self.assertEqual(True, Channel.objects.filter(channel_name=self.channel_name2).exists())
+        self.assertEqual(True, Thread.objects.filter(channel__channel_name=self.channel_name2, thread_id=thread_id2).exists())
+        self.assertEqual(True, Comment.objects.filter(thread__channel__channel_name=self.channel_name2, thread__thread_id=otherthread_id, comment_id=comment_id2).exists())
+
 
 #Confirm primary keys work as expected (esp. since comment has 3 primary keys and thread has 2)
 class UniqueValidationTests(ValidationErrorTestMixin, TestCase):
