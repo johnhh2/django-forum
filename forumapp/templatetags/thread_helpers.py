d456eff8492cbbd39f9ee7efe579c588e385b327
johnhh2 <johnhugh1998@gmail.com>
2020-06-10 20:05:36 +0000

    fixed delete button not showing for owners' channels

--- forumapp/templatetags/thread_helpers.py
+++ forumapp/templatetags/thread_helpers.py
@@ -7,17 +7,18 @@ register = template.Library()
 #Create filter for threads to see if they are owned by the user passed in
 @register.filter
 def is_owner(kwargs, user):
-    channel = Channel.objects.filter(channel_name=kwargs['channel_name'])
+    channel = Channel.objects.filter(channel_name=kwargs.get('channel'))
 
     if channel.exists():
-        return owner.get() == user
-    else:
-        return False
+        return channel.get().owner == user
+    
+    return False
 
 #Custom filter for threads to return the channel they belong to's description
 @register.filter
 def description(channel_name):
     channel = Channel.objects.filter(channel_name=channel_name)
+
     if channel.exists():
         return channel.get().description
     else:
@@ -26,8 +27,10 @@ def description(channel_name):
 @register.filter
 def is_moderator(kwargs, user):
     channel = Channel.objects.filter(channel_name=kwargs.get('channel'))
+    
     if channel.exists():
         channel = channel.get()
+        
         if user == channel.owner:
             return True
         else:
